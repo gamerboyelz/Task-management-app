8b325d24b643465997ca841766ce07f0d607482a
gamerboyelz <81883477+gamerboyelz@users.noreply.github.com>
2024-01-16 01:00:43 -0500

    functions were written to handle deleting and editing tasks

--- app.js
+++ app.js
@@ -24,9 +24,39 @@ const formValidation = () => {
     }
     
 }
+//look over from here
 let data = {}
 
 const acceptData = ()=>{
     data["text"] = input.value
     console.log (data)
+
+    createTask()
+}
+
+//create function
+const createTask = ()=>{
+    tasks. innerHTML += `
+    <div>
+        <p>${data.text}</p>
+        <span class= "options">
+            <i onclick = "editTasks(this)" class="fa-solid fa-file-pen"></i>
+            <i onclick = "deleteTasks(this)" class="fa-solid fa-trash"></i>
+
+        </span>
+    </div>
+    `
+    input.value = "";
+}
+
+// the delete task function is targetting the parent element 
+const deleteTasks = (element)=>{
+    element.parentElement.parentElement.remove()
+
+}
+
+const editTasks = (element)=>{
+    input.value = element.parentElement.previousElementSibling.innerHTML
+    element.parentElement.parentElement.remove()
+
 }
